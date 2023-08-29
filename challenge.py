5e2d347504c6b1aef5b0677dd806f6f078770c4f
Georgian Ghelase <ghelase.georgian@gmail.com>
2023-08-29 17:57:09 +0100

    2nd commit

--- challenge.py
+++ challenge.py
@@ -1,1 +1,7 @@
 # create a function that takes the RGB values of a colour and converts it to the corresponding HEX value for that colour.
+# the function should take 3 arguments, the red value, the green value and the blue value. 
+
+
+def rgb_to_hex(red, green, blue):
+    """this function converts RGB values into HEX values"""
+    
