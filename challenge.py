c8ddc90ddb3232bbe7881d2d3455f46e97a34966
Georgian Ghelase <ghelase.georgian@gmail.com>
2023-08-29 18:15:47 +0100

    3rd commit - asking for RGB values and raise error

--- challenge.py
+++ challenge.py
@@ -2,6 +2,10 @@
 # the function should take 3 arguments, the red value, the green value and the blue value. 
 
 
-def rgb_to_hex(red, green, blue):
+def rgbToHex(red, green, blue):
     """this function converts RGB values into HEX values"""
-    
+    question1 = input('Please enter the the RGB values: ')
+    question2 = input('Please enter the second RGB value: ')
+    question3 = input('Please enter the third RGB value: ')
+    if question1 & question2 & question3 < 0 or question1 & question2 & question3 > 1000:
+        raise Exception('Sorry, you have to enter a valid number')
