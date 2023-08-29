f65871b677f0c1aa1c412a8589b284ba17511196
Georgian Ghelase <ghelase.georgian@gmail.com>
2023-08-29 18:56:24 +0100

    5th commit

--- challenge.py
+++ challenge.py
@@ -14,12 +14,19 @@ def rgbToHex(red, green, blue):
     """this function converts RGB values into HEX values"""
 
     # we first ask the user for the input
-    question1 = int(input('Please enter the RGB values: '))
-    question2 = int(input('Please enter the second RGB value: '))
-    question3 = int(input('Please enter the third RGB value: '))
+    red = int(input('Please enter the RGB values: '))
+    green = int(input('Please enter the second RGB value: '))
+    blue = int(input('Please enter the third RGB value: '))
 
     # raise error if the input is different
-    if question1 & question2 & question3 < 0 or question1 & question2 & question3 > 255:
+    if red & blue & green < 0 or red & blue & green > 255:
         raise Exception('Sorry, you have to enter a valid number')
     
-    return f"{question1}, {question2}, {question3}"
+    RGB = f"{red}, {green}, {blue}"
+
+    # to return the right hexadecimal values, we have to use the following formula in 'return ...'
+    """
+    Given RGB values, where all three are 0-255 (0, 0, 0 being black and 255, 255, 255 being white),
+    use the equation: R/16 = x + y/16. G/16 = x' + y'/16. B/16 = x" + y"/16.
+    """
+    return RGB//16 =  x + y//16
